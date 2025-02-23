7ab3ac213d7fbab6de765011d1601bfc87d5b09e
Tomislav Celic <celer.tomislav@gmail.com>
2025-02-23 21:53:09 +0100

    Add url for static files

--- illness_predictor/urls.py
+++ illness_predictor/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from .views import illness_predictor_view
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
-    path('', illness_predictor_view, name="illness_predictor")
+    path('', illness_predictor_view, name="illness_predictor"),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
 ]
