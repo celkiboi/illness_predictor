6d6f3565e591f1962a51e2e0bb271aa8bac8bc27
AnteDragun <adragun@etfos.hr>
2025-02-23 21:33:19 +0100

    Update allowed hosts in settings.py

--- illness_predictor/settings.py
+++ illness_predictor/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-e+&*02xr(j2mi7x%g+-vgtjk_tump1!ml#fnwr8fpb=w4@p+7%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://illness-predictor.azurewebsites.net/']
+ALLOWED_HOSTS = ['illness-predictor.azurewebsites.net']
 
 
 # Application definition
