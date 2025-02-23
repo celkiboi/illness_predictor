39df282d03593e7a432bd500450520b11d587da9
Tomislav Celic <celer.tomislav@gmail.com>
2025-02-23 21:25:33 +0100

    Allow debug

--- illness_predictor/settings.py
+++ illness_predictor/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e+&*02xr(j2mi7x%g+-vgtjk_tump1!ml#fnwr8fpb=w4@p+7%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
